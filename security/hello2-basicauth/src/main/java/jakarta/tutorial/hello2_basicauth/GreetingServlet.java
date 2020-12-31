d722c07c2e2f427ca2bc2d6d244304fe04e9e122
hussainm <hussain.nm@cognizant.com>
2020-12-31 21:02:40 +0530

    Migrate security to jakartaee9
    Signed-off-by: hussainm <hussain.nm@cognizant.com>

--- security/hello2-basicauth/src/main/java/jakarta/tutorial/hello2_basicauth/GreetingServlet.java
+++ security/hello2-basicauth/src/main/java/jakarta/tutorial/hello2_basicauth/GreetingServlet.java
@@ -12,15 +12,16 @@ package jakarta.tutorial.hello2_basicauth;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.ServletSecurity;
-import javax.servlet.annotation.ServletSecurity.TransportGuarantee;
-import javax.servlet.annotation.HttpConstraint;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.ServletSecurity.TransportGuarantee;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "GreetingServlet", urlPatterns = {"/greeting"})
 @ServletSecurity(
